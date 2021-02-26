2567e051bfefea16f2e992d987c76469e57c9975
Bahadır Kisbet <bahadirkisbet@gmail.com>
2021-02-26 22:03:17 +0300

    Tree structure is being implemented,yet not completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 const PIXI = require('pixi.js'); // the graphic engine
 const Viewport = require('pixi-viewport').Viewport; // the dynamic background
+const anime = require('animejs');
+
 const app = new PIXI.Application({
     height : window.innerHeight - 53,
     width : window.innerWidth,
@@ -74,60 +76,113 @@ then use the template html file and import this js file there.
 
 /* Binary Search Tree */
 
-function calc_line_coor(x1,y1,x2,y2,radius) {
-    const hypotenuse = Math.sqrt( (y2-y1)**2 + (x2-x1)**2 );
-    const bottom = (x2-x1);
-    const side = (y2-y1);
-    
-    const cosinus = bottom / hypotenuse;
-    const sinus = side / hypotenuse;
+// CONSTANTS ARE HERE
 
-    var result = []
+COLOR_CONSTANTS = {
+    node: 0xff0000,
+    text: 0xffffff,
+    edge: 'black'
+};
 
-    result.push(x1 + radius*cosinus);
-    result.push(y1 + radius*sinus);
-
-    result.push(x2 - radius*cosinus);
-    result.push(y2 - radius*sinus);
-
-    return result;
-    
-}
-
-function draw_line(x1, y1, x2, y2, r) {
-    let line = new PIXI.Graphics();
-    line.lineStyle(2, 'black');
-    line.beginFill();
-    var points = calc_line_coor(x1,y1,x2,y2,r);
-    line.position.set(points[0],points[1]);
-    line.lineTo(points[2],points[3]);
-    viewport.addChild(line);
-}
+SIZE_CONSTANTS = {
+    radius: 30
+};
 
 
-var first = draw_circle([window.innerWidth / 2, 300], 0xff0000, text = "1");
-function draw_circle(position, color = 0xff0000, text) {
-    let circle = new PIXI.Graphics();
-    circle.lineStyle(2, color);
-    circle.beginFill(color);
-    circle.position.set(...position);
-    circle.drawCircle(0, 0, 30);
-    circle.endFill();
-    viewport.addChild(circle);
-    let style = new PIXI.TextStyle({ fill: ["0xffffff"] })
-    let txt = new PIXI.Text(text, style);
-    txt.anchor.set(0.5);
-    circle.addChild(txt);
-    return circle
-}
-var arr = []
-arr.push(first);
+
 function add_a_child() {
     var data = document.getElementById("add_input").value;
-    var prev_node = arr[arr.length - 1];
-    var curr_node = draw_circle([prev_node.x, prev_node.y + 100], 0xff0000, text = data);
-    arr.push(curr_node);
-    draw_line(prev_node.x, prev_node.y, prev_node.x, prev_node.y + 100);
-    if (data === "10") curr_node.destroy();
+    var prev_node;
+    var curr_node;
+
+    if(arr.length === 0) {
+        curr_node = draw_circle([window.innerWidth / 2, 300], 0xff0000, text = data);
+        arr.push(curr_node);
+    }
+    else {
+        
+            prev_node = arr[arr.length - 1];
+            curr_node = draw_circle([prev_node.x, prev_node.y + 100], 0xff0000, text = data);
+            arr.push(curr_node);
+            draw_line(prev_node.x, prev_node.y, prev_node.x, prev_node.y + 100, 30);
+    }
+    
+
 }
 
+// TODO - depthe göre boyut
+// TODO - root baz alınarak yeniden boyutlama
+
+class BinarySearchTreeNode {
+    constructor(_parent, _val, _left = null, _right = null) {
+        this.parent = _parent;
+        this.left = _left; 
+        this.right = _right;  // its type is a line or an arrow
+        this.val = _val;
+        this.circle = null;
+        this.edge = null;
+    }
+
+    draw_node(pos, color = COLOR_CONSTANTS.node) {
+        let circle = new PIXI.Graphics();
+        this.circle = circle;
+        circle.lineStyle(2, color);
+        circle.beginFill(color);
+        circle.position.set(...pos);
+        circle.drawCircle(0, 0, SIZE_CONSTANTS.radius);
+        circle.endFill();
+        viewport.addChild(circle);
+        let style = new PIXI.TextStyle({ fill: [COLOR_CONSTANTS.text] });
+        let txt = new PIXI.Text(text, style);
+        txt.anchor.set(0.5);
+        circle.addChild(txt);
+        return circle;
+
+    }
+
+    draw_edge() {
+        let line = new PIXI.Graphics();
+        this.edge = line;
+        line.lineStyle(2, COLOR_CONSTANTS.edge).beginFill();
+        var points = calc_line_coor(x1, y1, x2, y2, r);
+        line.position.set(points[0], points[1]);
+        line.lineTo(points[2] - points[0], points[3] - points[1]);
+        viewport.addChild(line);
+    }
+
+    calc_line_coor(x1, y1, x2, y2, radius) {
+        var hypotenuse = Math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2);
+
+        var bottom = (x2 - x1);
+        var side = (y2 - y1);
+
+        var cosinus = bottom / hypotenuse;
+        var sinus = side / hypotenuse;
+
+
+        var result = [
+            x1 + radius * cosinus,
+            y1 + radius * sinus,
+            x2 - radius * cosinus,
+            y2 - radius * sinus
+        ];
+
+        return result;
+
+    }
+    
+    set_pos(pos) {
+        this.circle.position.set(pos);
+    }
+
+    set_edge_post(pos) {
+        this.edge.lineTo(pos[0]-this.edge.x,pos[1]-this.edge.y);
+    }   
+
+};
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+};
